bd82173b0e2cd75ec02169b2b0451404f53f555f
AbedAwaisy <abed.awaisy.a@gmail.com>
2024-05-23 14:29:25 +0300

    create serivce file & update script

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,14 @@ cd ~/app
 # Ensure all dependencies are installed
 pip install -r requirements.txt
 
-# run the app
-python3 app.py
-#
+# Copy the service file to the systemd directory
+sudo cp new-flaskapp.service /etc/systemd/system/new-flaskapp.service
+
+# Reload the systemd daemon to recognize the new service
+sudo systemctl daemon-reload
+
+# Enable the service to start on boot
+sudo systemctl enable new-flaskapp.service
+
+# Restart the service to apply the latest changes
+sudo systemctl restart new-flaskapp.service
